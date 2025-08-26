facbc1446427804b065bf7ba2312e8b5c56fa37c
joseandreaslie <jose.lie2208@gmail.com>
2025-08-26 11:27:45 +0700

    Update TripDetailViewModel.swift

--- Coco/Pages/MyTrip/Detail/TripDetailViewModel.swift
+++ Coco/Pages/MyTrip/Detail/TripDetailViewModel.swift
@@ -121,7 +121,7 @@ extension TripDetailViewModel: TripDetailViewModelProtocol {
             event.title = self.data?.activityTitle ?? "Coco Activity"
             event.startDate = self.data?.date
 //            event.endDate = event.startDate.addingTimeInterval(3600*24)
-            event.endDate = Calendar.current.date(byAdding: .day, value: 1, to: event.startDate)!
+            event.endDate = Calendar.current.date(byAdding: .day, value: 0, to: event.startDate)
             event.isAllDay = true
             event.calendar = eventStore.defaultCalendarForNewEvents
             
